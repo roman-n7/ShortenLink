a3659c2aa9f88b563bff74192f74bdd6dbf8c5c1
Roman Navarrete <romansaballanavarrete@gmail.com>
2025-07-20 10:17:50 -0400

    created project

---
+++ src/main/java/com/roman_n7/ShortenLink/ShortenLinkApplication.java
@@ -0,0 +1,13 @@
+package com.roman_n7.ShortenLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman_n7/ShortenLink/ShortenLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman_n7.ShortenLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
